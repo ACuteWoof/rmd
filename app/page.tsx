317d30cc8241628846f55921a9f1ea5221d50c56
ACuteWoof <sutharshanvithushan@gmail.com>
2024-10-11 22:28:59 +0530

    feat(header): hide header based on search params

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import Render from "./renderer/render";
 import RenderClient from "./renderer/renderclient";
 
 type Props = {
-  params: { url: string; html: string; nossr: string };
+  params: { url: string; html: string; nossr: string; noheader: string };
   searchParams: { [key: string]: string | undefined };
 };
 
@@ -31,22 +31,25 @@ export default async function Page({ searchParams }: Props) {
     "https://raw.githubusercontent.com/ACuteWoof/rmd/refs/heads/main/README.md";
 
   const nossr: boolean = searchParams.nossr === "true";
+  const noheader: boolean = searchParams.noheader === "true";
 
   const res = await fetch(url as string);
   const thetext = await res.text();
 
   return (
     <main className={"w-full " + serif.className}>
-      <header className="py-4 px-8 flex justify-between">
-        <div />
-        <Link target="_blank" href={url as string}>
-          View raw
-        </Link>
-      </header>
+      {!noheader && (
+        <header className="py-4 px-8 flex justify-between">
+          <div />
+          <Link target="_blank" href={url as string}>
+            View raw
+          </Link>
+        </header>
+      )}
       <div className="px-8 py-12 w-full">
         <article className="mx-auto prose prose-neutral dark:prose-invert text-wrap break-words">
           {nossr ? (
-            <RenderClient searchParams={searchParams} url={url}/>
+            <RenderClient searchParams={searchParams} url={url} />
           ) : (
             <Render thetext={thetext} searchParams={searchParams} />
           )}
